0433b0ba795678e5212d07218b357e33e851a294
pbf6g4z5hs-boop <pbf6g4z5hs@privaterelay.appleid.com>
2026-05-29 14:42:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "/Valune <3",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
