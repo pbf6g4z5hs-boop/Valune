662eede5e66edb52165c6b31a8663239cc75b31b
pbf6g4z5hs-boop <pbf6g4z5hs@privaterelay.appleid.com>
2026-05-29 14:31:33 +0200

    Update bot activity name to '/Valune <3'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "/Valune <3",
         // Activity type number (0 = Playing).
         type: 0, 
       },
