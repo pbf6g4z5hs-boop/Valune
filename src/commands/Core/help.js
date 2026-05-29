477fc808eb16fc7b0550036f8a511398babbbb63
pbf6g4z5hs-boop <pbf6g4z5hs@privaterelay.appleid.com>
2026-05-29 14:50:06 +0200

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -158,7 +158,7 @@ export async function createInitialHelpMenu(client) {
     );
 
     embed.setFooter({ 
-        text: "Made with ❤️" 
+        text: "Valune <3" 
     });
     embed.setTimestamp();
 
@@ -167,15 +167,6 @@ export async function createInitialHelpMenu(client) {
         .setLabel("Report Bug")
         .setStyle(ButtonStyle.Danger);
 
-    const supportButton = new ButtonBuilder()
-        .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
-        .setStyle(ButtonStyle.Link);
-
-    const touchpointButton = new ButtonBuilder()
-        .setLabel("Learn from Touchpoint")
-        .setURL("https://www.youtube.com/@TouchDisc")
-        .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
         CATEGORY_SELECT_ID,
